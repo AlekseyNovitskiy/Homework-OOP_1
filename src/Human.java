b8d3e1757a07deb15c215a957ce80c6e1b95987a
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-10-19 19:43:20 +0300

    ООП. Урок 2 ДЗ готово

--- src/Human.java
+++ src/Human.java
@@ -1,17 +1,19 @@
+import java.time.LocalDateTime;
+
 public class Human {
-    int currentYear=2022;
-    int yearOfBirth;
+    int currentYear= LocalDateTime.now().getYear();
+    private int yearOfBirth;
     String name;
-    String town;
+    private String town;
     String jobTitle;
 
     public Human() {
     }
 
-    void conclusion() {
+    public void conclusion() {
         System.out.println("Привет! Меня зовут "+ name+ ". Я из города "
-                + town+ ". Я родился в "+ (currentYear-yearOfBirth)+
-                " году. Я работаю на должности "+jobTitle+". Будем знакомы!");
+                + getTown()+ ". Я родился в "+ getYearOfBirth()+
+                " году. Я сейчас нигде "+jobTitle+". Будем знакомы!");
     }
     public Human(String name,String town,int yearOfBirth,String jobTitle) {
         if (name == null) {
@@ -19,17 +21,19 @@ public class Human {
         } else {
             this.name = name;
         }
+
         if (town == null) {
             this.town = "Информация не указана";
         } else {
             this.town = town;
         }
-
-        if (yearOfBirth < 0) {
-            this.currentYear = 0;
+        if (yearOfBirth <= 0) {
+            this.yearOfBirth = 0;
         } else {
-            this.yearOfBirth = yearOfBirth;
+            this.yearOfBirth = currentYear - yearOfBirth;
         }
+
+
         if (jobTitle == null) {
             this.jobTitle = "Информация не указана";
         } else {
@@ -39,7 +43,33 @@ public class Human {
     }
     public String toString() {
         return "Привет! Меня зовут "+ name+ ". Я из города "
-                + town+ ". Я родился в "+ (currentYear-yearOfBirth)+
+                + getTown()+ ". Я родился в "+ getYearOfBirth()+
                 " году. Я работаю на должности "+jobTitle+". Будем знакомы!";
     }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth <= 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = currentYear-yearOfBirth;
+        }
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if (town != null && !name.isEmpty()) {
+            this.town = town;
+        } else {
+            this.town = "Информация не указана";
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
+        /*
         // Задание 1.
         System.out.println("Задание 1");
 
@@ -32,6 +35,8 @@ public class Main {
         artem.jobTitle = "директор по развитию бизнеса";
         artem.conclusion();
 
+         */
+
 
         // Задание 2.
         System.out.println("Задание 2");
@@ -40,6 +45,7 @@ public class Main {
         Human anya2 = new Human("Аня","Москва",29,"методист образовательных программ");
         Human katia2 = new Human("Катя","Калининград",28,"продакт-менеджер");
         Human artem2 = new Human("Максим","Минск",27,"директор по развитию бизнеса");
+
         System.out.println(maksim2);
         System.out.println(anya2);
         System.out.println(katia2);
@@ -53,12 +59,82 @@ public class Main {
         Human anya3 = new Human("Аня",null,29,"методист образовательных программ");
         Human katia3 = new Human("Катя","Калининград",28,null);
         Human artem3 = new Human("Максим","Минск",-27,"директор по развитию бизнеса");
+
         System.out.println(maksim3);
         System.out.println(anya3);
         System.out.println(katia3);
         System.out.println(artem3);
 
 
+        // ООП. УРОК 2. Задание 1.1
+        System.out.println("ООП. УРОК 2.  Задание 1.1");
+
+        Human vladimir = new Human("Владимир","Казань",21,"не работаю");
+        vladimir.conclusion();
+        vladimir.setTown(null);
+        vladimir.setYearOfBirth(-23);
+        vladimir.conclusion();
+
+        // ООП. УРОК 2. Задание 1.2
+        System.out.println("ООП. УРОК 2.  Задание 1.2");
+
+        Flower rose = new Flower("Роза обыкновенная",null,"Голландия",35.59,0);
+        Flower chrysanthemum = new Flower("Хризантема", null,null,15,5);
+        Flower pion = new Flower("Пион",null,"Англия",69.9,1);
+        Flower gypsophila = new Flower("Гипсофила",null,"Турция",19.5,10);
+        System.out.println(rose);
+        System.out.println(chrysanthemum);
+        System.out.println(pion);
+        System.out.println(gypsophila);
+        Flower[] flowers = {rose,rose,rose,chrysanthemum,chrysanthemum,chrysanthemum,chrysanthemum,chrysanthemum,gypsophila};
+        printBouquet(flowers);
+    }
+
+    private static void printBouquet(Flower[] flowers) {
+        double totalCost = 0;
+        int minimumLifeSpan = Integer.MAX_VALUE;
+        int countRose = 0;
+        int countChrysanthemum = 0;
+        int countPion = 0;
+        int countGypsophila = 0;
+        for (int i = 0; i < flowers.length; i++) {
+            if (flowers[i].lifeSpan < minimumLifeSpan) {
+                minimumLifeSpan = flowers[i].lifeSpan;
+            }
+            if (flowers[i].name.equals("Роза обыкновенная")) {
+                countRose++;
+            }
+            else if (flowers[i].name.equals("Хризантема")) {
+                countChrysanthemum++;
+            }
+            else if (flowers[i].name.equals("Пион")) {
+                countPion++;
+            }
+            else if (flowers[i].name.equals("Гипсофила")) {
+                countGypsophila++;
+            }
+
+                totalCost += flowers[i].getCost();
+
+        }
+        totalCost*=1.1;
+        System.out.print("Букет состоит из: ");
+        if (countRose > 0) {
+            System.out.print(countRose + " роз, ");
+        }
+        if (countChrysanthemum > 0) {
+            System.out.print(countChrysanthemum +  " хризантем, ");
+        }
+        if (countPion > 0) {
+            System.out.print(countPion +  " пионов, ");
+        }
+        if (countGypsophila > 0) {
+            System.out.print(countGypsophila +  " гипсофил. ");
+        }
+        System.out.println();
+        String resultCost = String.format("%.2f",totalCost);
+        System.out.println("Стоимость букета "  + resultCost+", срок стояния букета "+minimumLifeSpan+" дня.");
 
     }
+
 }
